a85a44f79f5e8f7c89b4f7f6ff30594e3e2b0a7a
Henrioque Souza <henrique.asouza@outlook.com>
2021-10-24 10:02:24 -0300

    chore: structuring project

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,37 @@
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Home from "./components/pages/Home";
+import Company from "./components/pages/Company";
+import Contact from "./components/pages/Contact";
+import NewProject from "./components/pages/NewProject";
+import Container from "./components/layout/Container";
+
 function App() {
   return (
-    <div className="App">
-      <p>Costs</p>
-    </div>
+    <Router>
+      <div>
+        <Link to="/">Home</Link>
+        <Link to="/company">Company</Link>
+        <Link to="/contact">Contact</Link>
+        <Link to="/newproject">New Project</Link>
+      </div>
+      <Switch>
+        <Container customClass="min-height">
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/company">
+            <Company />
+          </Route>
+          <Route path="/contact">
+            <Contact />
+          </Route>
+          <Route path="/newproject">
+            <NewProject />
+          </Route>
+        </Container>
+      </Switch>
+      <p>Footer</p>
+    </Router>
   );
 }
 
