37795f869a3f083bb5db81363e0f80c53a9647bd
Henrioque Souza <henrique.asouza@outlook.com>
2021-10-24 20:56:17 -0300

    feat: navbar and footer along formating in project code

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,24 @@
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./components/pages/Home";
 import Company from "./components/pages/Company";
 import Contact from "./components/pages/Contact";
 import NewProject from "./components/pages/NewProject";
+import Projects from "./components/pages/Projects";
 import Container from "./components/layout/Container";
+import Navbar from "./components/layout/Navbar";
+import Footer from "./components/layout/Footer";
 
 function App() {
   return (
     <Router>
-      <div>
-        <Link to="/">Home</Link>
-        <Link to="/company">Company</Link>
-        <Link to="/contact">Contact</Link>
-        <Link to="/newproject">New Project</Link>
-      </div>
+      <Navbar />
       <Switch>
         <Container customClass="min-height">
           <Route exact path="/">
             <Home />
+          </Route>
+          <Route path="/projects">
+            <Projects />
           </Route>
           <Route path="/company">
             <Company />
@@ -25,12 +26,9 @@ function App() {
           <Route path="/contact">
             <Contact />
           </Route>
-          <Route path="/newproject">
-            <NewProject />
-          </Route>
         </Container>
       </Switch>
-      <p>Footer</p>
+      <Footer />
     </Router>
   );
 }
--- src/components/pages/NewProject.jsx
+++ src/components/pages/NewProject.jsx
@@ -1,5 +1,5 @@
 function NewProject() {
-    return <h1>NewProject</h1>
+  return <h1>NewProject</h1>;
 }
 
-export default NewProject
+export default NewProject;
