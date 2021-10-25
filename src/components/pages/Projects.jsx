121081577e644a28c240ed2d4494d1783c97101a
Henrique Souza <henrique.souza@ngi.com.br>
2021-10-25 13:52:23 -0300

    feat: project card
    created projects are now displayed in projects page

--- src/components/pages/Projects.jsx
+++ src/components/pages/Projects.jsx
@@ -3,13 +3,31 @@ import { useLocation } from "react-router-dom";
 import styles from "./Projects.module.css";
 import Container from "../layout/Container";
 import LinkButton from "../layout/LinkButton";
+import ProjectCard from "../project/ProjectCard";
+import { useState, useEffect } from "react";
 
 function Projects() {
+  const [projects, setProjects] = useState([]);
+
   const location = useLocation();
   let message = "";
   if (location.state) {
     message = location.state.message;
   }
+
+  useEffect(() => {
+    fetch("http://localhost:5000/projects", {
+      method: "GET",
+      headers: {
+        "Content-type": "application/json",
+      },
+    })
+      .then((resp) => resp.json())
+      .then((data) => {
+        setProjects(data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
 
   return (
     <div className={styles.project_container}>
@@ -19,7 +37,16 @@ function Projects() {
       </div>
       {message && <Message msg={message} type="success" />}
       <Container customClass="start">
-        <p>Projects...</p>
+        {projects.length > 0 &&
+          projects.map((project) => (
+            <ProjectCard
+              id={project.id}
+              name={project.name}
+              budget={project.budget}
+              category={project.category.name}
+              key={project.id}
+            />
+          ))}
       </Container>
     </div>
   );
---
+++ src/components/project/ProjectCard.jsx
@@ -0,0 +1,24 @@
+import styles from "./ProjectCard.module.css";
+import { Link } from "react-router-dom";
+import { BsPencil, BsTrashFill } from "react-icons/bs";
+function ProjectCard({ id, name, budget, category, handleRemove }) {
+  return (
+    <div className={styles.project_card}>
+      <h4>{name}</h4>
+      <p>
+        <span>Budget:</span> U${budget}
+      </p>
+      <p className={styles.category_text}>
+        <span className={styles[category.toLowerCase()]}></span> {category}
+      </p>
+      <div className={styles.project_card_actions}>
+        <Link to="/">
+          <BsPencil /> Edit
+        </Link>
+        <button><BsTrashFill /> Remove</button>
+      </div>
+    </div>
+  );
+}
+
+export default ProjectCard;
