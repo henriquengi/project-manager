7b5d8f37e4224f6329407882a751919ed964e673
Henrique Souza <henrique.souza@ngi.com.br>
2021-10-25 10:01:31 -0300

    chore: fake api, select with options

--- src/components/form/Select.jsx
+++ src/components/form/Select.jsx
@@ -6,6 +6,9 @@ function Select({ text, name, handleOnChange, options, value }) {
       <label htmlFor={name}>{text}:</label>
       <select name={name} id={name}>
         <option>Select a option</option>
+        {options.map((option) => (
+          <option value={option.id} key={option.id}>{option.name}</option>
+        ))}
       </select>
     </div>
   );
--- src/components/project/ProjectForm.jsx
+++ src/components/project/ProjectForm.jsx
@@ -2,8 +2,25 @@ import styles from "./ProjectForm.module.css";
 import Input from "../form/Input";
 import Select from "../form/Select";
 import SubmitButton from "../form/SubmitButton";
+import { useState, useEffect } from "react";
 
 function ProjectForm({ btnText }) {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/categories", {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((result) => result.json())
+    .then((data) => {
+      setCategories(data);
+    })
+    .catch((err) => console.log(err));
+  }, [])
+
   return (
     <form action="" className={styles.form}>
       <Input
@@ -18,7 +35,7 @@ function ProjectForm({ btnText }) {
         name="budget"
         placeholder="Insert budget"
       />
-      <Select name="category_id" text="Select category" />
+      <Select name="category_id" text="Select category" options={categories} />
       <SubmitButton text={btnText} />
     </form>
   );
