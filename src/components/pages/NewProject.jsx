1e63fa581fd8b0733aecb0709a459f0963093dd8
Henrique Souza <henrique.souza@ngi.com.br>
2021-10-25 08:49:30 -0300

    feat: new project page

--- src/components/pages/NewProject.jsx
+++ src/components/pages/NewProject.jsx
@@ -1,5 +1,14 @@
+import styles from "./NewProject.module.css"
+import ProjectForm from "../project/ProjectForm";
+
 function NewProject() {
-  return <h1>NewProject</h1>;
+  return (
+    <div className={styles.newproject_container}>
+      <h1>Create Project</h1>
+      <p>Create a project to add services</p>
+      <ProjectForm />
+    </div>
+  )
 }
 
 export default NewProject;
